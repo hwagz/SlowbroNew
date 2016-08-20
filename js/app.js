adb0f6996ebfed435a9632a1d8198887ebf3875c
hwagz <hunt.wagner@gmail.com>
2016-08-20 14:48:48 -0400

    Adjusted some names

--- js/app.js
+++ js/app.js
@@ -2,36 +2,36 @@ var app = angular.module('angtemp',['ngRoute']);
 
 app.config(function($routeProvider){
     $routeProvider
-    .when('/url1',{
-      templateUrl: "views/url1.htm",
-      controller: "url1Ctrl"
+    .when('/pokemon',{
+      templateUrl: "views/pokemon.htm",
+      controller: "pokemonCtrl"
     })
-    .when('/url2',{
-      templateUrl: "views/url2.htm",
-      controller: "url2Ctrl"
+    .when('/wow',{
+      templateUrl: "views/wow.htm",
+      controller: "wowCtrl"
     })
     .when('/url3',{
       templateUrl: "views/url3.htm",
       controller: "url3Ctrl"
     })
     .otherwise({
-      templateUrl: "views/main.htm",
-      controller: "mainCtrl"
+      templateUrl: "views/home.htm",
+      controller: "homeCtrl"
     });
   })
-  .controller('mainCtrl', ['$scope','$filter','$location',function($scope, $filter, $location) {
-    $scope.page = "Main";
-    $scope.msg = "This is the main page."
+  .controller('homeCtrl', ['$scope','$filter','$location',function($scope, $filter, $location) {
+    $scope.page = "Home";
+    $scope.msg = "This is the home page."
     $scope.btn = function(){
       console.log($location.absUrl());
     }
   }])
-  .controller('url1Ctrl', ['$scope','$filter',function($scope, $filter) {
-    $scope.page = "URL 1";
+  .controller('pokemonCtrl', ['$scope','$filter',function($scope, $filter) {
+    $scope.page = "Pokemon";
     $scope.msg = "Test message 1";
   }])
-  .controller('url2Ctrl', ['$scope','$filter',function($scope, $filter) {
-    $scope.page = "URL 2";
+  .controller('wowCtrl', ['$scope','$filter',function($scope, $filter) {
+    $scope.page = "World of Warcraft";
     $scope.msg = "Test message 2";
   }])
   .controller('url3Ctrl', ['$scope','$filter',function($scope, $filter) {
