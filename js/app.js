f3763f0bf1608722790cd7dcac0b8a2809d3b693
hwagz <hunt.wagner@gmail.com>
2016-08-23 22:14:59 -0400

    Making progress. API http req added

--- js/app.js
+++ js/app.js
@@ -29,8 +29,43 @@ app.config(function($routeProvider){
   .controller('pokemonCtrl', ['$scope','$filter',function($scope, $filter) {
     $scope.page = "Pokemon";
   }])
-  .controller('wowCtrl', ['$scope','$filter',function($scope, $filter) {
+  .controller('wowCtrl', ['$scope','$filter','$http',function($scope, $filter,$http) {
     $scope.page = "World of Warcraft";
+    $scope.url1 ="https://us.api.battle.net/wow/auction/data/";
+    $scope.realm = "nazjatar";
+    $scope.url2 = $scope.realm+"?locale=en_US&jsonp=true&apikey=";
+    $scope.urlKey = "";
+    $scope.haveData = false;
+
+    $scope.url = $scope.url1+$scope.url2+$scope.urlKey;
+
+    // Get list of all site groups user has access to
+    $scope.getData = function(){
+      $http({
+        method: 'GET',
+        url: $scope.url
+      })
+        .then(function(response) {
+          console.log(response);
+          var urlStr = response.data;
+          if (!$scope.haveData) {
+            $scope.haveData = true;
+            $scope.url = $scope.cleanURL(response.data);
+            $scope.getData();
+          }
+        })
+        .finally(function(){
+          console.log("Complete.");
+        });
+    };
+
+    // dig the actual url out of the flippin' "data" string. dumb.
+    $scope.cleanURL = function(responseStr){
+      var startIndex = responseStr.indexOf("http");
+      var newURL = responseStr.substring(startIndex);
+      var endIndex = newURL.indexOf('","');
+      return newURL.substring(0,endIndex);
+    };
   }])
   .filter('capitalize', function() {
     return function(input) {
