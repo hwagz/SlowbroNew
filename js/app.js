4f499ee0c326b0a4ee794ab48d7e545684534a31
hwagz <hunt.wagner@gmail.com>
2016-08-25 22:58:35 -0400

    Did some styling, fiddled with WoW API only to find it's actually broken. yay. Working on style/responsiveness for nwo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+"use strict";
+
+// <520px width => center slowbro circle, nav list dropdown
+
 var app = angular.module('slowbro',['ngRoute']);
 
 app.config(function($routeProvider){
@@ -29,42 +33,55 @@ app.config(function($routeProvider){
   .controller('pokemonCtrl', ['$scope','$filter',function($scope, $filter) {
     $scope.page = "Pokemon";
   }])
-  .controller('wowCtrl', ['$scope','$filter','$http',function($scope, $filter,$http) {
+  .controller('wowCtrl', ['$scope','$filter','$http',function($scope,$filter,$http) {
     $scope.page = "World of Warcraft";
     $scope.url1 ="https://us.api.battle.net/wow/auction/data/";
     $scope.realm = "nazjatar";
-    $scope.url2 = $scope.realm+"?locale=en_US&jsonp=true&apikey=";
-    $scope.urlKey = "";
+    $scope.url2 = $scope.realm+"?locale=en_US&jsonp=JSON_CALLBACK&apikey=";
+    $scope.urlKey = "8xez9wkqrw7tdqmsvbsmnszz6z3kek39";
     $scope.haveData = false;
 
     $scope.url = $scope.url1+$scope.url2+$scope.urlKey;
 
     // Get list of all site groups user has access to
+    // WORKING
     $scope.getData = function(){
       $http({
-        method: 'GET',
+        method: 'JSONP',
         url: $scope.url
       })
-        .then(function(response) {
-          console.log(response);
-          var urlStr = response.data;
-          if (!$scope.haveData) {
+        .then(
+          function(response) {
             $scope.haveData = true;
-            $scope.url = $scope.cleanURL(response.data);
-            $scope.getData();
-          }
+            var lastUpdated = new Date(response.data.files[0].lastModified);
+            console.log(lastUpdated);
+            $scope.url = response.data.files[0].url;
+          },
+          function(response){
+          console.log("Error");
+          console.log(response);
         })
         .finally(function(){
-          console.log("Complete.");
+          console.log("HTTP req 1 Complete.");
+          if ($scope.haveData) {
+            console.log($scope.url);
+            $http({
+              method:'GET',
+              url: $scope.url
+            })
+              .then(
+                function(response) {
+                  //console.log(response);
+                },
+                function(response){
+                  console.log("Error 2: The Sequel");
+                  console.log(response);
+                })
+              .finally(function(){
+                console.log("Complete.");
+              });
+          }
         });
-    };
-
-    // dig the actual url out of the flippin' "data" string. dumb.
-    $scope.cleanURL = function(responseStr){
-      var startIndex = responseStr.indexOf("http");
-      var newURL = responseStr.substring(startIndex);
-      var endIndex = newURL.indexOf('","');
-      return newURL.substring(0,endIndex);
     };
   }])
   .filter('capitalize', function() {
